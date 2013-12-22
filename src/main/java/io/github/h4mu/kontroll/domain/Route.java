63cd63179ceadd4a224d0faf5faf00760a1ef42f
Tamas Hamor <hamuguy@gmail.com>
2013-12-23 00:32:12 +0100

    design improvements

--- src/main/java/io/github/h4mu/kontroll/domain/Route.java
+++ src/main/java/io/github/h4mu/kontroll/domain/Route.java
@@ -35,7 +35,7 @@ public class Route {
 		Calendar timeNow = Calendar.getInstance();
 		timeNow.set(Calendar.YEAR, epoch.get(Calendar.YEAR));
 		timeNow.set(Calendar.DAY_OF_YEAR, epoch.get(Calendar.DAY_OF_YEAR));
-        TypedQuery<Route> query = entityManager.createQuery("SELECT DISTINCT o FROM Trip t INNER JOIN t.route o WHERE :timeNow BETWEEN t.startTime AND t.endTime ORDER BY o.shortName", Route.class);
+        TypedQuery<Route> query = entityManager.createQuery("SELECT DISTINCT o FROM Trip t INNER JOIN t.route o WHERE :timeNow BETWEEN t.startTime AND t.endTime ORDER BY o.color, o.textColor, o.shortName", Route.class);
         query.setParameter("timeNow", timeNow.getTime());
         return query;
     }
