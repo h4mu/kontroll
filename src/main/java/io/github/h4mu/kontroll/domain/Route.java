0b96e8c1d5cc380e75ce93cf38fd10e2e218d787
Tamas Hamor <hamuguy@gmail.com>
2013-12-30 22:49:02 +0100

    improved time filtering

--- src/main/java/io/github/h4mu/kontroll/domain/Route.java
+++ src/main/java/io/github/h4mu/kontroll/domain/Route.java
@@ -1,8 +1,4 @@
 package io.github.h4mu.kontroll.domain;
-import org.springframework.roo.addon.javabean.RooJavaBean;
-import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
-import org.springframework.roo.addon.tostring.RooToString;
-
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.List;
@@ -14,6 +10,10 @@ import javax.persistence.OneToMany;
 import javax.persistence.TypedQuery;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
 
 @RooJavaBean
 @RooToString
@@ -33,10 +33,14 @@ public class Route {
         EntityManager entityManager = entityManager();
 		Calendar epoch = Calendar.getInstance();
 		epoch.setTimeInMillis(0);
+        TypedQuery<Route> query = entityManager.createQuery("SELECT DISTINCT o FROM Trip t INNER JOIN t.route o "
+			+ "WHERE :timeNow BETWEEN t.startTime AND t.endTime " 
+	        + "OR (t.startTime > t.endTime AND " 
+			+ "(:timeNow <= t.startTime OR :timeNow >= t.endTime)) " 
+	        + "ORDER BY o.color, o.textColor, o.shortName", Route.class);
 		Calendar timeNow = Calendar.getInstance();
 		timeNow.set(Calendar.YEAR, epoch.get(Calendar.YEAR));
 		timeNow.set(Calendar.DAY_OF_YEAR, epoch.get(Calendar.DAY_OF_YEAR));
-        TypedQuery<Route> query = entityManager.createQuery("SELECT DISTINCT o FROM Trip t INNER JOIN t.route o WHERE :timeNow BETWEEN t.startTime AND t.endTime ORDER BY o.color, o.textColor, o.shortName", Route.class);
         query.setParameter("timeNow", timeNow.getTime());
         return query.getResultList();
     }
--- src/main/java/io/github/h4mu/kontroll/domain/Trip.java
+++ src/main/java/io/github/h4mu/kontroll/domain/Trip.java
@@ -51,7 +51,11 @@ public class Trip {
 		Calendar timeNow = Calendar.getInstance();
 		timeNow.set(Calendar.YEAR, epoch.get(Calendar.YEAR));
 		timeNow.set(Calendar.DAY_OF_YEAR, epoch.get(Calendar.DAY_OF_YEAR));
-        TypedQuery<Trip> query = route.entityManager.createQuery("SELECT o FROM Trip AS o WHERE o.route = :route AND :timeNow BETWEEN o.startTime AND o.endTime ORDER BY o.headSign", Trip.class);
+        TypedQuery<Trip> query = route.entityManager.createQuery("SELECT t FROM Trip AS t WHERE t.route = :route " 
+			+ "AND (:timeNow BETWEEN t.startTime AND t.endTime " 
+	        + "OR (t.startTime > t.endTime AND " 
+			+ "(:timeNow <= t.startTime OR :timeNow >= t.endTime))) " 
+    		+ "ORDER BY t.headSign", Trip.class);
         query.setParameter("route", route);
         query.setParameter("timeNow", timeNow.getTime());
         return query.getResultList();
